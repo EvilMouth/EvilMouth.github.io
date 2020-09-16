d154cfc53b046de233d2e70b82ccd10e5909c6d5
EVIL MOUTH <izyhang@gmail.com>
2020-09-16 14:25:18 +0800

    index ul

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,24 @@
 import React from "react"
 
 const Index = ({ data, location }) => {
-  return <p>Hello</p>
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="/blog">read Blog</a>
+        </li>
+        <li>
+          <a href="https://github.com/EvilMouth">fork Github</a>
+        </li>
+        <li>
+          <a href="https://twitter.com/EvilLittleMouth">mention Twitter</a>
+        </li>
+        <li>
+          <a href="https://telegram.me/EvilLittleMouth">chat Telegram</a>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Index
